135b640ffb5bb393aff1c18fe56d89a3cc4d6cf1
Krisztian Ivan <krisztian.ivan@gmail.com>
2022-08-25 10:04:18 +0100

    try recognise speech

--- voice_recognition.py
+++ voice_recognition.py
@@ -1,7 +1,7 @@
 import speech_recognition as sr
 
 def recognise_speech_from_mic(recogniser, microphone):
-    #check that recogniser and microphone are appropriate SpeechRecognition instances
+    # check that recogniser and microphone are appropriate SpeechRecognition instances
     if not isinstance(recogniser, sr.Recogniser):
         raise TypeError("recogniser must be a SpeechRecognition Recogniser instance")
 
@@ -12,3 +12,23 @@ def recognise_speech_from_mic(recogniser, microphone):
     with microphone as source:
         recogniser.adjust_for_ambient_noise(source)
         audio = recogniser.listen(source)
+
+    # initialise response object
+    response = {
+        "success": True,
+        "error": None,
+        "transcription": None
+    }
+
+    # try recognising speech from mic recording
+    try:
+        response["transcription"] = recogniser.recognise_google(audio)
+    except sr.RequestError:
+        # API unreachable or unresponsive
+        response["success"] = False
+        response["error"] = "API unavailable"
+    except sr.UnknownValueError:
+        # speech unintelligible
+        response["error"] = "Unable to recognise speech"
+
+    return response
